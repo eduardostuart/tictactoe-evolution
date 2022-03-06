eb2723101446b09c7c9d491119f4dc9dbcf4e72c
Eduardo Stuart <hi@s.tuart.me>
2022-03-06 18:57:55 +0100

    update text - winner dialog

--- src/components/WinnerDialog.tsx
+++ src/components/WinnerDialog.tsx
@@ -12,7 +12,7 @@ export const WinnerDialog: FC<WinnerProps> = ({ winner }) => {
     <div className="fixed z-50 bg-black/30 left-0 right-0 bottom-0 flex items-center justify-center top-0 w-full h-full ">
       <div className="bg-white border-5 border-gray-600 shadow text-lg rounded-lg text-center">
         <div className="p-4">
-          <h3 className="text-4xl font-black mb-6">Game is over</h3>
+          <h3 className="text-4xl font-black mb-6">Game Over</h3>
           {winner === "Tie" ? (
             "Tie!"
           ) : (
@@ -28,7 +28,7 @@ export const WinnerDialog: FC<WinnerProps> = ({ winner }) => {
             onClick={() => game.reset()}
             className="bg-blue-500 hover:bg-blue-800 py-2 px-4 rounded-lg text-white font-semibold border-2 border-blue-300"
           >
-            New game
+            Play again
           </a>
         </div>
       </div>
